73ab98fdb317acde2d86f064b77f64d283b21ca8
Scrambled Onion <scrambledonion2222@gmail.com>
2022-07-18 07:33:20 -0400

    Testing

--- HaloModHelper/MainWindow.Designer.cs
+++ HaloModHelper/MainWindow.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace HaloModHelper
 {
-    partial class Form1
+    partial class MainWindow
     {
         /// <summary>
         /// Required designer variable.
@@ -29,7 +29,7 @@ namespace HaloModHelper
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
             this.singleMultiSelect = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
--- HaloModHelper/MainWindow.cs
+++ HaloModHelper/MainWindow.cs
@@ -11,14 +11,14 @@ using System.Windows.Forms;
 
 namespace HaloModHelper
 {
-    public partial class Form1 : Form
+    public partial class MainWindow : Form
     {
         public static string modFile;
         public static string mapFolder;
         public static string backupFolder;
         public static string mapFile;
 
-        public Form1()
+        public MainWindow()
         {
             InitializeComponent();
         }
@@ -242,7 +242,7 @@ namespace HaloModHelper
 
             if (mapFile.Equals(""))
             {
-                MessageBox.Show("Select what level you are overwriting", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("Select what level ou are overwriting", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
 
             if(backupBox.Checked == true)
